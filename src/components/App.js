4b1b2ac40617cb8995d09d80bc7520284cb9a901
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2019-06-29 22:02:45 +0200

    Authentication stuff.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 import Header from './Header';
-import { Switch, Route } from "react-router-dom";
+import Login from './Login';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
